a867af9cdb0c76798611aa5b29a0b1b5a4fa52ca
Sixclones <roch.bouchayer@gmail.com>
2018-12-03 19:00:56 +0100

    navheader and a bit of style

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,15 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Accueil</router-link>
-      <router-link to="/presidents">Présidents</router-link>
-      <router-link to="/parties">Parties</router-link>
-      <router-link to="/laws">Lois</router-link>
-      <router-link to="/map">Carte</router-link>
-    </div>
-    <router-view/>
+    <NavHeader />
+    <RouterView class="page" />
   </div>
 </template>
+<script>
+import NavHeader from '@/components/NavHeader.vue';
+
+export default {
+  components: { NavHeader }
+};
+</script>
 
 <style>
+@import url('./assets/css/reset.css');
+
+html {
+  --black: #333;
+  --dark-purple: #5603ad;
+  --light-purple: #8367c7;
+  --green: #b3e9c7;
+  --white: #f0fff1;
+
+  --header-height: 50px;
+}
+
+body {
+  font-family: monospace;
+  background: #fff;
+}
+
+.page {
+  max-width: 1280px;
+  width: 90%;
+  margin: calc(2 * var(--header-height)) auto;
+}
 </style>
---
+++ client/src/components/NavHeader.vue
@@ -0,0 +1,75 @@
+<template>
+  <nav class="nav-header">
+    <ul class="list">
+      <li class="list__item">
+        <RouterLink
+          to="/"
+          class="list__item__link"
+        >
+          Accueil
+        </RouterLink>
+      </li>
+      <li class="list__item">
+        <RouterLink
+          to="/presidents"
+          class="list__item__link"
+        >
+          Présidents
+        </RouterLink>
+      </li>
+      <li class="list__item">
+        <RouterLink
+          to="/parties"
+          class="list__item__link"
+        >
+          Parties
+        </RouterLink>
+      </li>
+      <li class="list__item">
+        <RouterLink
+          to="/laws"
+          class="list__item__link"
+        >
+          Lois
+        </RouterLink>
+      </li>
+      <li class="list__item">
+        <RouterLink
+          to="/map"
+          class="list__item__link"
+        >
+          Carte
+        </RouterLink>
+      </li>
+    </ul>
+  </nav>
+</template>
+<style scoped>
+.nav-header {
+  width: 100%;
+  height: var(--header-height);
+  position: fixed;
+  top: 0;
+  left: 0;
+  border-bottom: 1px solid var(--black);
+}
+
+.list {
+  height: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0 12px;
+}
+.list__item {
+  margin: 0 12px;
+}
+.list__item__link {
+  padding: 5px 0;
+  color: var(--black);
+  text-decoration: none;
+}
+.list__item__link:hover,
+.list__item__link--active {
+  border-bottom: 2px solid var(--dark-purple);
+}
+</style>
