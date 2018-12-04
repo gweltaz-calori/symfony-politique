cbe70e82a7466dd28201a8a70cd7a79afb57cf99
Sixclones <roch.bouchayer@gmail.com>
2018-12-04 19:52:19 +0100

    feat(api:

---
+++ client/src/api/index.js
@@ -0,0 +1,59 @@
+const config = {
+  baseURL: 'http://politic.gweltaz-calori.com'
+};
+
+function computeURI(path, params) {
+  if (!params) {
+    return path;
+  }
+  let realPath;
+  Object.keys(params).forEach(key => {
+    realPath = path.replace(`:${key}`, params[key]);
+  });
+  return realPath;
+}
+
+async function get(path, data) {
+  const uri = data ? computeURI(path, data.params) : path;
+  return await fetch(`${config.baseURL}${uri}`).then(res => res.json());
+}
+
+async function post(path, data) {
+  const uri = data ? computeURI(path, data.params) : path;
+  return await fetch(`${config.baseURL}${uri}`, {
+    method: 'POST',
+    body: data
+  }).then(res => res.json());
+}
+
+export const endpoints = {
+  fetchPresidents: { method: get, path: '/presidents' },
+  postPresidents: { method: post, path: '/presidents' },
+  fetchPresident: { method: get, path: '/presidents/:presidentId' },
+  fetchPresidentCoordinates: {
+    method: get,
+    path: '/presidents/:presidentId/coordinates'
+  },
+  fetchPresidentLaws: { method: get, path: '/presidents/:presidentId/laws' },
+  postPresidentLaws: { method: post, path: '/presidents/:presidentId/laws' },
+  getPresidentLaws: {
+    method: get,
+    path: '/presidents/:presidentId/laws/:lawId'
+  },
+
+  fetchLaws: { method: get, path: '/laws' },
+  fetchLaw: { method: get, path: '/laws/:law' },
+  fetchLawsVotes: { method: get, path: '/laws/:law/votes' },
+  postLawsVotes: { method: post, path: '/laws/:law/votes' },
+
+  fetchPersons: { method: get, path: '/persons' },
+  postPersons: { method: post, path: '/persons' },
+  fetchPerson: { method: get, path: '/persons/:personId' }
+};
+
+const api = {};
+Object.keys(endpoints).forEach(key => {
+  api[key] = data => endpoints[key].method(endpoints[key].path, data);
+});
+
+export default api;
