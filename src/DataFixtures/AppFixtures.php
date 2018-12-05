3cf7d384e6d8a3cbe7b467950a384775bcd74f8c
gweltaz-calori <andoracontact@gmail.com>
2018-12-05 18:15:23 +0100

    feat - more fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\PoliticalParty;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -12,6 +13,10 @@ class AppFixtures extends Fixture
         $president = new \App\Entity\President();
         $president->setName("Macron");
         $president->setCountry("France");
+
+        $party = new PoliticalParty();
+        $party->setName("En marche");
+        $president->setPoliticalParty($party);
 
         $manager->persist($president);
 
@@ -24,6 +29,64 @@ class AppFixtures extends Fixture
 
         $person = new \App\Entity\Person();
         $person->setName("Bob");
+
+        $manager->persist($person);
+
+        $vote = new \App\Entity\LawVote();
+        $vote->setPerson($person);
+
+        $law->setVotes([$vote]);
+
+        $manager->persist($vote);
+
+        $president = new \App\Entity\President();
+        $president->setName("Trump");
+        $president->setCountry("UnitedStates");
+
+        $party = new PoliticalParty();
+        $party->setName("Republicans");
+        $president->setPoliticalParty($party);
+
+        $manager->persist($president);
+
+        $law = new \App\Entity\Law();
+        $law->setDescription("Mur du mexique");
+        $law->setCreatedBy($president);
+
+        $manager->persist($law);
+        $president->setLaws([$law]);
+
+        $person = new \App\Entity\Person();
+        $person->setName("Tom");
+
+        $manager->persist($person);
+
+        $vote = new \App\Entity\LawVote();
+        $vote->setPerson($person);
+
+        $law->setVotes([$vote]);
+
+        $manager->persist($vote);
+
+        $president = new \App\Entity\President();
+        $president->setName("Angela Merkel");
+        $president->setCountry("Allemagne");
+
+        $party = new PoliticalParty();
+        $party->setName("Union chrétienne-démocrate d'Allemagne");
+        $president->setPoliticalParty($party);
+
+        $manager->persist($president);
+
+        $law = new \App\Entity\Law();
+        $law->setDescription("Biere pour tous");
+        $law->setCreatedBy($president);
+
+        $manager->persist($law);
+        $president->setLaws([$law]);
+
+        $person = new \App\Entity\Person();
+        $person->setName("Hanz");
 
         $manager->persist($person);
 
