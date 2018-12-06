75b795e83a5747a8e73cbf8564acd0a49b7a60da
Sixclones <roch.bouchayer@gmail.com>
2018-12-06 12:50:28 +0100

    refactor(CardList)

---
+++ client/src/components/CardList.vue
@@ -0,0 +1,73 @@
+<template>
+  <ul class="list">
+    <li
+      v-for="item in list"
+      :key="item.key"
+      class="list__item"
+    >
+      <RouterLink
+        :to="`/presidents/${item.uuid}`"
+        class="list__item__link card"
+      >
+        <img
+          v-if="item.image"
+          :src="item.image"
+          class="card__image"
+        >
+        <p class="card__name">
+          {{ item.name }}
+        </p>
+      </RouterLink>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      required: true
+    }
+  }
+};
+</script>
+<style scoped>
+.list {
+  display: flex;
+  flex-flow: wrap;
+  justify-content: space-between;
+}
+
+.list__item {
+  width: 48%;
+  margin: 12px 0;
+}
+
+.list__item__link {
+  display: block;
+  font-size: 18px;
+  color: var(--black);
+  text-decoration: none;
+}
+
+.card {
+  display: flex;
+  align-items: center;
+  padding: 18px 20px;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.18);
+  transition: box-shadow 0.3s;
+}
+.card:hover {
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+}
+.card__image {
+  width: 50px;
+  height: 50px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+.card__name {
+  margin-left: 12px;
+}
+</style>
--- client/src/views/Presidents.vue
+++ client/src/views/Presidents.vue
@@ -1,32 +1,15 @@
 <template>
   <div class="page">
     <PageTitle label="Liste des présidents" />
-    <ul class="list">
-      <li
-        v-for="president in presidents"
-        :key="president.key"
-        class="list__item"
-      >
-        <RouterLink
-          :to="`/presidents/${president.uuid}`"
-          class="list__item__link card"
-        >
-          <img
-            :src="president.image"
-            class="card__image"
-          >
-          <p class="card__name">
-            {{ president.name }}
-          </p>
-        </RouterLink>
-      </li>
-    </ul>
+    <CardList :list="presidents" />
   </div>
 </template>
 <script>
 import PageTitle from '@/components/PageTitle';
+import CardList from '@/components/CardList';
+
 export default {
-  components: { PageTitle },
+  components: { PageTitle, CardList },
   data() {
     return {
       presidents: this.$store.state.presidents
@@ -34,43 +17,3 @@ export default {
   }
 };
 </script>
-<style scoped>
-.list {
-  display: flex;
-  flex-flow: wrap;
-  justify-content: space-between;
-}
-
-.list__item {
-  width: 48%;
-  margin: 12px 0;
-}
-
-.list__item__link {
-  display: block;
-  font-size: 18px;
-  color: var(--black);
-  text-decoration: none;
-}
-
-.card {
-  display: flex;
-  align-items: center;
-  padding: 18px 20px;
-  border-radius: 5px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.18);
-  transition: box-shadow 0.3s;
-}
-.card:hover {
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
-}
-.card__image {
-  width: 50px;
-  height: 50px;
-  object-fit: cover;
-  border-radius: 50%;
-}
-.card__name {
-  margin-left: 12px;
-}
-</style>
