60bb7486ceaf5acc9aef122ebb1fe1c0512496af
Sixclones <roch.bouchayer@gmail.com>
2018-12-06 19:26:13 +0100

    feat(SheetHeader)

---
+++ client/src/components/SheetHeader.vue
@@ -0,0 +1,91 @@
+<template>
+  <header class="header">
+    <div class="header__part header__part--left">
+      <img
+        :src="image"
+        class="header__image"
+      >
+      <h1 class="header__title">
+        {{ title }}
+      </h1>
+    </div>
+    <div class="header__part header__part--right">
+      <p class="header__subtitle">
+        {{ subtitle }}
+      </p>
+      <p class="header__counter">
+        {{ counter }}
+      </p>
+    </div>
+  </header>
+</template>
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    subtitle: {
+      type: String,
+      required: true
+    },
+    counter: {
+      type: Number,
+      required: true
+    }
+  }
+};
+</script>
+<style scoped>
+.header {
+  max-width: var(--ctt-width);
+  width: 90%;
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: auto;
+  padding: 12px 16px;
+}
+.header::before {
+  content: '';
+  width: 100vw;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.18);
+}
+.header__part {
+  display: flex;
+  align-items: center;
+}
+.header__part--right {
+  flex-direction: column;
+}
+.header__subtitle {
+  font-size: 16px;
+  color: var(--dark-grey);
+}
+.header__counter {
+  font-size: 30px;
+  font-weight: bold;
+}
+.header__image {
+  width: 75px;
+  height: 75px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+.header__title {
+  margin-left: 20px;
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
--- client/src/views/President.vue
+++ client/src/views/President.vue
@@ -1,24 +1,11 @@
 <template>
   <div v-if="president">
-    <header class="header">
-      <div class="header__part header__part--left">
-        <img
-          :src="president.image"
-          class="header__image"
-        >
-        <h1 class="header__name">
-          {{ president.name }}
-        </h1>
-      </div>
-      <div class="header__part header__part--right">
-        <p class="header__part__subtitle">
-          {{ lawsCount > 1 ? 'Lois' : 'Loi' }}
-        </p>
-        <p class="header__part__number">
-          {{ lawsCount }}
-        </p>
-      </div>
-    </header>
+    <SheetHeader
+      :image="president.image"
+      :title="president.name"
+      :subtitle="lawsCount > 1 ? 'Lois' : 'Loi'"
+      :counter="lawsCount"
+    />
     <div class="content">
       <ul class="list">
         <p class="list__title">
@@ -73,10 +60,11 @@
   </div>
 </template>
 <script>
+import SheetHeader from '@/components/SheetHeader';
 import SvgIcon from '@/components/SvgIcon';
 
 export default {
-  components: { SvgIcon },
+  components: { SheetHeader, SvgIcon },
   computed: {
     president() {
       return this.$store.state.presidents.find(
@@ -95,53 +83,6 @@ export default {
 };
 </script>
 <style scoped>
-.header {
-  max-width: var(--ctt-width);
-  width: 90%;
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin: auto;
-  padding: 12px 16px;
-}
-.header::before {
-  content: '';
-  width: 100vw;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.18);
-}
-.header__part {
-  display: flex;
-  align-items: center;
-}
-.header__part--right {
-  flex-direction: column;
-}
-.header__part__subtitle {
-  font-size: 16px;
-  color: var(--dark-grey);
-}
-.header__part__number {
-  font-size: 30px;
-  font-weight: bold;
-}
-.header__name {
-  margin-left: 20px;
-  font-size: 20px;
-  font-weight: bold;
-}
-.header__image {
-  width: 75px;
-  height: 75px;
-  object-fit: cover;
-  border-radius: 50%;
-}
-
 .content {
   max-width: var(--ctt-width);
   width: 90%;
