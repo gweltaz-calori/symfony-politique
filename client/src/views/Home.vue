89f04cf2206819db8c6abde9eb084e0754ea23e2
Sixclones <roch.bouchayer@gmail.com>
2018-12-07 18:57:50 +0100

    feat(Home): some text

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="Page">
-    Ce site permet de visualiser les différents présidents de différents pays.
+  <div class="Page home">
+    <p class="text">
+      Vous trouverez sur ce site une liste de différents présidents, de leur partie respectifs et des lois qu'ils proposent.
+    </p>
+    <p class="text">
+      Ce projet est un exercice dans le cadre du workshop Symfony à l'école des Gobelins. Il a été réalisé par Gweltaz Calori et Roch Bouchayer sous l'enseignemant de Sebastien Fleury.
+    </p>
   </div>
 </template>
 <script>
@@ -8,3 +13,15 @@ export default {
   name: 'Home'
 };
 </script>
+<style scoped>
+.home {
+  width: var(--ctt-width);
+}
+.text {
+  margin: 15px 0;
+  line-height: 1.5;
+}
+.text:first-child {
+  margin-top: 0;
+}
+</style>
